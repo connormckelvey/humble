3b4bb98d0de2bc41ae06ad8ed9a7b37d6ff014c6
Connor McKelvey <connormckelvey@gmail.com>
2015-10-26 21:46:38 -0600

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  gulp.src('./src/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/**/*.scss', ['sass']);
+});
